34f5b5c6e673bbc1c04cdc861f7c024d1b41ea07
Sohail Hosseini <S.SADATHOSSEINI@GMAIL.COM>
2023-08-06 19:12:21 -0400

    first commit

---
+++ diabetes_logistic_regression.py
@@ -0,0 +1,24 @@
+# Import necessary libraries
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+# Load the dataset
+data_path = "diabetes.csv"  
+diabetes_data = pd.read_csv(data_path)
+
+# Split the data into training and testing sets
+X = diabetes_data.drop("Outcome", axis=1)
+y = diabetes_data["Outcome"]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Initialize and train the logistic regression model
+logreg = LogisticRegression(max_iter=1000)
+logreg.fit(X_train, y_train)
+
+# Predict and evaluate the model's performance
+y_pred = logreg.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+
+print("Accuracy:", accuracy)
